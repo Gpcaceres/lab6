cb545e6a3a28b86a685801d565ac73307cc49486
Germán Cáceres <gpcaceres@espe.edu.ec>
2025-08-07 12:36:07 -0500

    pfpfpfsss

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 export default [
   {
-    files: ["**/*.js"], // cualquier archivo js
+    files: ['**/*.js'], // cualquier archivo js
     languageOptions: {
       ecmaVersion: 'latest', // última versión de ECMAScript (nota: era 'lastest', debe ser 'latest')
       sourceType: 'module' // tipo de módulo
